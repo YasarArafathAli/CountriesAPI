b49019ab3894e1b736b2273b1e8916dc3e68f9c4
Yasar Arafath <yasara218@outlook.com>
2021-09-05 19:16:51 +0530

    flag layout

--- src/card.js
+++ src/card.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import "./card.css";
 function Card(props) {
-  
+
+  const getCountryname = (code) => {
+    fetch(`https://restcountries.eu/rest/v2/alpha/${code}`)
+      .then(res => res.json())
+      .then(data => {
+      console.log(data);
+    })
+  }  
 
     return (
       <div className="cards-item">
         <div className="card-image">
           
-          <img className={ props.country.name + 'flag'}  src={props.country.flag} alt={props.country.cioc + "flag"} width="fit-content" height ="200"></img></div>
+          <img className={ props.country.alpha2Code + 'flag'}  src={props.country.flag} alt={props.country.cioc + "flag"} ></img></div>
         <div className="card-content">
           <div className="card-title"><h2>{props.country.name.toUpperCase()}</h2></div>
           <div className="card-details">
@@ -15,7 +22,7 @@ function Card(props) {
             <p><strong>Region</strong> : {props.country.region}</p>
             <p><strong>Srb-region</strong> : {props.country.subregion}</p>
             <p><strong>Population</strong> : {props.country.population}</p>
-            <p><strong>Borders</strong> : {props.country.borders.join()}</p>
+            <p><strong>Borders</strong> : {props.country.borders.map(getCountryname)}</p>
             <p><strong>Languages</strong> : {props.country.languages.map(lang => lang.name).join()}</p>
           </div>
         </div>
