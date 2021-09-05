545ee6186c30f18286347181ac023fe20704fb56
Yasar Arafath <yasara218@outlook.com>
2021-09-05 19:36:57 +0530

    border set

--- src/card.js
+++ src/card.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import "./card.css";
 function Card(props) {
-
-  const getCountryname = (code) => {
-    fetch(`https://restcountries.eu/rest/v2/alpha/${code}`)
-      .then(res => res.json())
-      .then(data => {
-      console.log(data);
-    })
-  }  
-
     return (
       <div className="cards-item">
         <div className="card-image">
@@ -22,7 +13,7 @@ function Card(props) {
             <p><strong>Region</strong> : {props.country.region}</p>
             <p><strong>Srb-region</strong> : {props.country.subregion}</p>
             <p><strong>Population</strong> : {props.country.population}</p>
-            <p><strong>Borders</strong> : {props.country.borders.map(getCountryname)}</p>
+            <p><strong>Borders</strong> : {props.country.borders.join()}</p>
             <p><strong>Languages</strong> : {props.country.languages.map(lang => lang.name).join()}</p>
           </div>
         </div>
